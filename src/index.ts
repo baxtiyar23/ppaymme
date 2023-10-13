d27b24bda528d6556319254fff718d866b7995e1
Ruslan <irisbayevruslan2288@gmail.com>
2023-10-13 10:12:34 +0300

    ruslan codes

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/main.css";
+import * as yup from 'yup'; // Import Yup
 
 interface FormValues {
- email: string;
- password: string |number;
+  email: string;
+  password: string | number;
 }
 
-function validateForm(formValues: FormValues): boolean {
- if (formValues.email && formValues.password) {
-   return true;
- } else {
-   return false;
- }
-}
+// Define a Yup schema for validation
+const validationSchema = yup.object().shape({
+  email: yup.string().email().required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
 
 function parseFormValues(): FormValues {
- const emailInput = document.getElementById('exampleInputEmail1') as HTMLInputElement;
- const passwordInput = document.getElementById('exampleInputPassword1') as HTMLInputElement;
+  const emailInput = document.getElementById('exampleInputEmail1') as HTMLInputElement;
+  const passwordInput = document.getElementById('exampleInputPassword1') as HTMLInputElement;
 
- const formValues: FormValues = {
-   email: emailInput.value,
-   password: passwordInput.value,
- };
+  const formValues: FormValues = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
 
- return formValues;
+  return formValues;
 }
 
-function handleSubmit(event: Event) {
- event.preventDefault();
+async function handleSubmit(event: Event) {
+  event.preventDefault();
 
- const formValues = parseFormValues();
- const isValid = validateForm(formValues);
+  const formValues = parseFormValues();
 
- if (isValid) {
-   console.log('Form is valid');
-   console.log(formValues);
- }
+  try {
+    await validationSchema.validate(formValues, { abortEarly: false });
+    console.log('Form is valid');
+    console.log(formValues);
+  } catch (error) {
+    // Handle validation errors
+    console.error('Form validation failed', error);
+  }
 }
-
 
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
 
+// const passwordsection = document.querySelector('.password') as HTMLElement;
+// const first = document.getElementById('next') as HTMLButtonElement;
+// const emailsection = document.querySelector('.email') as HTMLElement;
 
-const passwordsection = document.querySelector(".password") as HTMLElement;
-const first = document.getElementById("next") as HTMLButtonElement;
-const emailsection = document.querySelector(".email") as HTMLElement;
-
-first.onclick =()=>{
-passwordsection.style.display="none";
-emailsection.style.display="block";
-}
+// first.onclick = () => {
+//   passwordsection.style.display = 'none';
+//   emailsection.style.display = 'block';
+// };
